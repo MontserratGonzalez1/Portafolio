8bb3af87a082bb5434d222c29b3a9effe5740749
MontserratGonzalez1 <monkygs@gmail.com>
2016-12-01 17:00:52 -0600

    cambie la seccion de gustos ya tiene modal

--- js/script.js
+++ js/script.js
@@ -6,7 +6,17 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-
+/* SLIDER */
 $(document).ready(function(){
       $('.slider').slider({full_width: true});
     });
+
+
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+       
+ $('#modal1').modal('open');
+ $('#modal1').modal('close');
